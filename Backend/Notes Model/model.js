b47c4471c1e6aded68acb7daae7618e6ce963a37
riya-rjha <riyaranjanjha2006@gmail.com>
2024-01-28 12:34:08 +0530

    Created a noteModel

---
+++ Backend/Notes Model/model.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const noteSchema = new Schema(
+    {
+        topic: {
+            type: String,
+            required: true,
+        },
+        status: {
+            type: String,
+            required: true,
+        },
+        notes: {
+            type: String,
+            required: true
+        }
+    },
+    {
+        timestamps: true,
+    }
+);
+
+export const Notes = mongoose.model('Cat',noteSchema);
--- Readme.md
+++ Readme.md
@@ -1,5 +1,3 @@
 # Notes-Taker
 
-This is a note taking application built from Scratch using reactjs and tailwind css to make the website responsive and easy to use. The databases used are from mongodb and stored in API Postman. The site's link is as follows: 
-
-``
+This is a note taking application built from Scratch using reactjs and tailwind css to make the website responsive and easy to use. The databases used are from mongodb and stored in API Postman. 
