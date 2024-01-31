05c2eb53b0e3639bc759f882dede1c7482134318
riya-rjha <riyaranjanjha2006@gmail.com>
2024-02-01 02:31:24 +0530

    Added Screenshot to gitignore file

--- Frontend/src/Pages/EditNotes.jsx
+++ Frontend/src/Pages/EditNotes.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'; 
+import axios from 'axios'
 
 const EditNotes = () => {
+
+  const [topic, setTopic] = useState('');
+  const [status, setStatus] = useState('');
+  const [notes, setNotes] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const navigate = useNavigate('');
+  const {id} = useParams();
+  const API_URl = `http://localhost:5555/notes/${id}`;
+  //Get book of specific Id
+
+  //Handle Edit button after getting book of particular ID
+
+
+  const handleEdit = async() => {
+    const data = {topic, status, notes};
+    await axios.put(API_URl, data);
+  }
   return (
-    <div>EditNotes</div>
+    <div>
+      
+    </div>
   )
 }
 
