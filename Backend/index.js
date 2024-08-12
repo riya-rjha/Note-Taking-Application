25abe2d428dc7a38fbff7991bbb93773d1632627
RRJ <riyaranjanjha2006@gmail.com>
2024-08-12 21:14:31 +0530

    Update backend code formatting and dependencies

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,9 @@
 //Express helps manage servers and routes
-import 'dotenv/config'
-import { mongoose } from 'mongoose';
+import "dotenv/config";
+import { mongoose } from "mongoose";
 import express from "express";
-import NotesRoute from './Routes/routes.js'
-import cors from 'cors'
+import NotesRoute from "./Routes/routes.js";
+import cors from "cors";
 
 const app = express();
 
@@ -13,24 +13,19 @@ app.use(cors());
 
 app.use(express.json());
 
-app.get('/', (request, response) => {
-    console.log(request);
-    return response.status(202).send('Welcome to my Note Making Application');
-});
-
-app.use('/notes', NotesRoute);
+app.use("/notes", NotesRoute);
 
 //Connecting to a Database (MongoDB)
 const connectToDatabase = async () => {
-    try {
-        await mongoose.connect(process.env.mongoDBURL);
-        console.log('Connected to Database');
-        app.listen(process.env.PORT, () => {
-            console.log(`App is listening to PORT ${process.env.PORT}`);
-        })
-    } catch (error) {
-        console.log(error.message);
-    }
-}
+  try {
+    await mongoose.connect(process.env.mongoDBURL);
+    console.log("Connected to Database");
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listening to PORT ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
-connectToDatabase();    
+connectToDatabase();
